1d27e8b3844873b44c04c2d332b53d4da99787f3
zaorjin <49592417+zaorjin@users.noreply.github.com>
2019-05-03 20:46:33 -0300

    Add files via upload

--- volgal.c
+++ volgal.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+short int cvogal(char c);
+
 int main(){
-    char nome[150], vogal[5]={'a','e','i','o','u'};
-    int tamanho,cont=0;
+    char nome[150], mvogais[150];
+    int tamanho,cont=0,c=0;
     memset(&nome,'\0' ,150);
+    memset(&mvogais,'\0' ,150);
     printf("Digite seu nome: ");
     scanf("%[^\n]s", nome);
     getchar();
-    tamanho=strlen(nome);
-    for(int i=0;i<tamanho;i++){
-        for(int c=0;c<5;c++){
-            if(nome[i]==vogal[c]){
-                cont++;
-            }
+    for(int i=0;i<strlen(nome);i++){
+        if(cvogal(nome[i])){
+            mvogais[c]=nome[i];
+            c++;
+            cont++;
         }
     }
-    printf("\nQuantidade de vogais: %d",cont);
 
+    printf("\nVogais: %s",mvogais);
+    printf("\nQuantidade de vogais: %d",cont);
     return(0);
 }
+
+short int cvogal(char c){
+    char vogal[5]={'a','e','i','o','u'};
+    for (int i=0; i<5; i++){
+        if(c==vogal[i]){
+            return(1);
+        }
+    }
+    return (0);
+    }
+
