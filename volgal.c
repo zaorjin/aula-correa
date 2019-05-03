eb3a48951a61b45f9b1fc34c338cd86fa19e7dd4
zaorjin <49592417+zaorjin@users.noreply.github.com>
2019-05-03 01:21:01 -0300

    Add files via upload

--- volgal.c
+++ volgal.c
@@ -2,20 +2,21 @@
 #include <ctype.h>
 #include <string.h>
 int main(){
-    char nome[150], vogal['a','e','i','o','u'];
+    char nome[150], vogal[5]={'a','e','i','o','u'};
     int tamanho,cont=0;
     memset(&nome,'\0' ,150);
     printf("Digite seu nome: ");
     scanf("%[^\n]s", nome);
+    getchar();
     tamanho=strlen(nome);
     for(int i=0;i<tamanho;i++){
         for(int c=0;c<5;c++){
             if(nome[i]==vogal[c]){
-                cont++
+                cont++;
             }
         }
     }
-
+    printf(cont);
 
     return(0);
 }
