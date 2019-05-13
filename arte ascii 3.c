6121606f1ebe594cfd5114d2bc5f2c540b1e8ad3
zaorjin <49592417+zaorjin@users.noreply.github.com>
2019-05-13 13:28:58 -0300

    Add files via upload

--- arte ascii 3.c
+++ arte ascii 3.c
@@ -2,7 +2,7 @@
 
 int main(){
     int l1=0,c1=0,n1,cc;
-    printf("\nInforme o tamanho da maior linha: ");
+    printf("\nInforme o tamanho da maior linha par: ");
     scanf("%d",&n1);
     cc=n1;
     do{
@@ -30,34 +30,35 @@ int main(){
             }
         }
         n1-=2;
-    }while(n1!=0);
-        while((n1<cc)||(n1==cc)){
-            c1=0;
-        if(cc==n1){
-            while(c1<n1){
-                printf("*");
-                c1++;
-            }
-        }else{
-            while(c1<n1/2){
-                printf("*");
-                c1++;
-            }
-            c1=0;
-            while(c1<cc-n1){
-                printf(" ");
-                c1++;
-            }
-            c1=0;
-            while(c1<n1/2){
-                printf("*");
-                c1++;
-            }
-        }
+    }while(n1>0);
 
-            n1+=2;
-            printf("\n");
-        }
+    while((n1<cc)||(n1==cc)){
+            c1=0;
+            if(cc==n1){
+                while(c1<n1){
+                    printf("*");
+                    c1++;
+                }
+            }else{
+                while(c1<n1/2){
+                    printf("*");
+                    c1++;
+                    }
+                c1=0;
+                while(c1<cc-n1){
+                    printf(" ");
+                    c1++;
+                }
+                c1=0;
+                while(c1<n1/2){
+                    printf("*");
+                    c1++;
+                }
+            }
+
+                n1+=2;
+                printf("\n");
+            }
 
  return(0);
 }
